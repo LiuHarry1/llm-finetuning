755c87a2a80274de4fed4c34a433cb691c389494
LiuHarry1 <593954214@qq.com>
2025-08-21 17:24:01 +0800

    ss

--- memory_learn/zep/case3.py
+++ memory_learn/zep/case3.py
@@ -1,28 +1,121 @@
 import os
+
+from langchain_community.retrievers import ZepCloudRetriever
 from zep_cloud.client import AsyncZep
-from zep_cloud import Message
+from zep_cloud import Message, ThreadGetUserContextRequestMode
+import asyncio
+#https://app.getzep.com/projects/ff41087d-e0f6-496d-8cea-e2e05be96d20/playground
 
-#https://zep.us/en/home/spaces
+zep_api_key = "z_1dWlkIjoiY2EzYzc1ZjMtYmM4My00N2I3LWFkZGYtNzBiM2NjMzM5NmRmIn0.3j67lAIWi8r5zVs9JChzXUHkGlYKZg74kEs_pUwUxhmDduj76IPLnDiVzG3MaapDGm8hdDLeH0GTxDuglH2A_w"
 
-# 初始化 Zep 客户端
-zep = AsyncZep(api_key=os.getenv("ZEP_API_KEY"))
+def test_zep1():
+    zep = AsyncZep(api_key="z_1dWlkIjoiY2EzYzc1ZjMtYmM4My00N2I3LWFkZGYtNzBiM2NjMzM5NmRmIn0.3j67lAIWi8r5zVs9JChzXUHkGlYKZg74kEs_pUwUxhmDduj76IPLnDiVzG3MaapDGm8hdDLeH0GTxDuglH2A_w")
 
-# 创建一个新的会话
-session_id = "user123-session"
-await zep.memory.create(session_id)
+    # print("Current Context:", memory.context)
 
-# 添加用户消息到记忆
-user_message = "Hi, I'm looking for a sci-fi movie recommendation."
-await zep.memory.add(session_id, messages=[Message(role="user", content=user_message)])
 
-# 获取当前会话的上下文
-memory = await zep.memory.get(session_id)
-print("Current Context:", memory.context)
+async def test_zep():
+    # 初始化 Zep 客户端
 
-# 模拟 AI 响应
-ai_response = "How about 'Dune'? It's a great sci-fi movie."
-await zep.memory.add(session_id, messages=[Message(role="assistant", content=ai_response)])
+    client = AsyncZep(api_key=zep_api_key)
 
-# 获取更新后的上下文
-updated_memory = await zep.memory.get(session_id)
-print("Updated Context:", updated_memory.context)
+    user_id = "user_123"
+    thread_id = "thread_123"
+
+    # 添加/更新用户信息
+    # await client.user.add(user_id=user_id, first_name="Harry")
+
+    # 创建线程
+    # await client.thread.create(thread_id=thread_id, user_id=user_id)
+
+    # 添加消息
+    # messages = [
+    #     Message(name="Harry", role="user", content="Hi, I'm looking for a sci-fi movie recommendation."),
+    #     Message(name="Assistant", role="assistant", content="How about 'Dune'? It's a great sci-fi movie.")
+    # ]
+    # await client.thread.add_messages(thread_id=thread_id, messages=messages)
+    #
+    # context = await client.thread.get_user_context(thread_id=thread_id)
+    # print("Context:", context)
+
+    results = await client.graph.search(user_id=user_id, query="Dune", limit=3)
+    print("Graph Facts:", [edge.fact for edge in results.edges])
+
+    results = await client.thread.search(
+        thread_id=thread_id,
+        query="sci-fi movie",
+        top_k=2
+    )
+    for m in results.messages:
+        print(m.role, ":", m.content)
+
+async def search_in_history():
+
+    user_id = "user_123"
+    thread_id = "thread_123"
+
+    # 初始化 Zep Cloud 客户端
+    client = AsyncZep(api_key=zep_api_key)
+
+
+    # 获取线程历史
+    context = await client.thread.get_user_context(thread_id=thread_id, mode="basic")
+
+    print("Context:", context)
+
+    context = await client.thread.get_user_context(thread_id=thread_id, mode="summary")
+
+    print("Context:", context)
+
+
+
+"""
+
+FACTS and ENTITIES represent relevant context to the current conversation.
+
+# These are the most relevant facts and their valid date ranges
+# format: FACT (Date range: from - to)
+<FACTS>
+  - How about 'Dune'? It's a great sci-fi movie. (2025-08-21 02:44:31 - present)
+  - Harry is looking for a sci-fi movie recommendation. (2025-08-21 02:59:55 - present)
+  - Hi, I'm looking for a sci-fi movie recommendation. (2025-08-21 02:44:31 - present)
+  - Dune is a sci-fi movie. (2025-08-21 02:58:26 - present)
+  - It's a great sci-fi movie. (2025-08-21 02:59:55 - present)
+</FACTS>
+
+# These are the most relevant entities
+# Name: ENTITY_NAME
+# Label: entity_label (if present)
+# Attributes: (if present)
+#   attr_name: attr_value
+# Summary: entity summary
+<ENTITIES>
+  - Name: sci-fi movie
+    Label: Topic
+    Attributes:
+      domain: sci-fi
+      expertise_level: movie
+    Summary: The user is looking for a sci-fi movie recommendation. The assistant suggested 'Dune'.
+  - Name: Harry
+    Label: User
+    Attributes:
+      email: 
+      first_name: Harry
+      last_name: 
+      role_type: user
+      user_id: user_123
+    Summary: user with the id of user_123, name of Harry. Harry is looking for a sci-fi movie recommendation.
+</ENTITIES>
+
+"""
+
+"""
+• Harry requested a sci-fi movie recommendation on 2025-08-21 02:44:31 (2025-08-21 02:44:31 - present)
+• The assistant recommended 'Dune' as a sci-fi movie on 2025-08-21 02:44:31 (2025-08-21 02:44:31 - present)
+• 'Dune' was described as a great sci-fi movie on 2025-08-21 02:59:55 (2025-08-21 02:59:55 - present)"
+
+"""
+
+if __name__ == '__main__':
+    asyncio.run(test_zep())
+    # asyncio.run(search_in_history())
---
+++ memory_learn/zep/case4.py
@@ -0,0 +1,126 @@
+
+from dotenv import load_dotenv
+import os
+
+# 加载 .env 文件中的变量
+load_dotenv(override=True)
+
+# 通过 os.getenv 获取
+ZEP_API_KEY = os.getenv("ZEP_API_KEY")
+
+print(ZEP_API_KEY)
+
+from zep_cloud.client import Zep
+
+
+def add_user(user_id):
+    client = Zep(api_key=ZEP_API_KEY)
+
+    new_user = client.user.add(
+        user_id=user_id,
+        email="user@example.com",
+        first_name="Harry",
+        last_name="Liu",
+    )
+    print(new_user)
+
+def get_user(user_id):
+    client = Zep(api_key=ZEP_API_KEY)
+    user = client.user.get(user_id)
+    print(user)
+    return user
+
+def update_user(user_id):
+
+    client = Zep(api_key=ZEP_API_KEY)
+    updated_user = client.user.update(
+        user_id=user_id,
+        email="updated_user@example.com",
+        first_name="Harry",
+        last_name="he",
+    )
+    print(updated_user)
+
+def delete_user(user_id):
+    client = Zep(api_key=ZEP_API_KEY)
+    client.user.delete(user_id)
+
+def get_threads(user_id):
+    client = Zep(api_key=ZEP_API_KEY)
+    threads = client.user.get_threads(user_id)
+
+    for thread in threads:
+        print(thread)
+
+def list_users():
+    client = Zep(api_key=ZEP_API_KEY)
+    result = client.user.list_ordered(page_size=10, page_number=1)
+    print(result)
+
+
+def get_user_node(user_id):
+    client = Zep(api_key=ZEP_API_KEY)
+    results = client.user.get_node(user_id=user_id)
+    user_node = results.node
+    print(user_node.summary)
+
+def find_node_by_graph(user_id):
+    client = Zep(api_key=ZEP_API_KEY)
+    # Initialize the Zep client
+
+    nodes = client.graph.node.get_by_user_id(user_id=user_id)
+    for node in nodes:
+        print(node)
+
+def search_in_graph(user_id, query):
+    client = Zep(api_key=ZEP_API_KEY)
+    results = client.graph.search(
+        user_id=user_id,
+        query=query,  # To help narrow down the nodes you have to manually search
+        scope="nodes",
+        min_score=1
+    )
+    relevant_nodes = results.nodes
+
+    for relevant_node in relevant_nodes:
+        print(relevant_node)
+
+def get_relevant_fact(user_id, query, center_node_uuid):
+    client = Zep(api_key=ZEP_API_KEY)
+    results = client.graph.search(
+        user_id=user_id,
+        query=query,
+        reranker="node_distance",
+        center_node_uuid=center_node_uuid,
+    )
+    relevant_edges = results.edges
+    relevant_facts = [edge.fact for edge in relevant_edges]
+
+    for relevant_fact in relevant_facts:
+        print(relevant_fact)
+
+
+def get_facts(user_id, center_node_uuid):
+    client = Zep(api_key=ZEP_API_KEY)
+    edges = client.graph.edge.get_by_user_id(user_id=user_id)
+    connected_edges = [edge for edge in edges if
+                       edge.source_node_uuid == center_node_uuid or edge.target_node_uuid == center_node_uuid]
+    relevant_facts = [edge.fact for edge in connected_edges]
+    for relevant_fact in relevant_facts:
+        print(relevant_fact)
+
+
+if __name__ == '__main__':
+    # user_id = "hl77319"
+    user_id = "user_123"
+    # add_user(user_id)
+    # update_user(user_id)
+    # delete_user(user_id)
+    # get_user(user_id)
+    # get_threads(user_id)
+    # list_users()
+    # get_user_node(user_id)
+    # find_node_by_graph(user_id)
+    # search_in_graph(user_id, "harry")
+    # get_facts(user_id, "d42ae90c-ecd2-4940-9377-f98c22ffe985")
+    get_relevant_fact(user_id,"harry", "d42ae90c-ecd2-4940-9377-f98c22ffe985" )
